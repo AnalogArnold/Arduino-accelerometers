6bba994ff7df189544f2df372002765bddeabff4
Michael Darcy <45762689+AnalogArnold@users.noreply.github.com>
2025-07-02 16:58:15 +0100

    Create app.py
    Add the main app file

---
+++ Python/app.py
@@ -0,0 +1,15 @@
+##################################################################################################################
+#
+# Main file for the accelerometer reader.
+#
+# Version: 2.0 (July 2025)
+# Author: Michael Darcy
+# License: MIT
+# Copyright (C) 2025 AnalogArnold
+#
+##################################################################################################################
+from gui import AccelerometerReaderGUI
+
+if __name__ == "__main__":
+    app = AccelerometerReaderGUI()
+    app.run()
